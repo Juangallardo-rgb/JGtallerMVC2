56a82ac5074310b41d19eb1bcbcd6234b4f15fef
Juan Esteban Gallardo Alava <75323396+Juangallardo-rgb@users.noreply.github.com>
2024-10-21 10:28:46 -0500

    Primer commit

---
+++ JGtallerMVC/Models/Burger.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JGtallerMVC.Models
+{
+    public class Burger
+    {
+            public int Id { get; set; }
+
+            [Required]
+            public string? Name { get; set; }
+            public bool WithCheese { get; set; }
+
+            [Range(0.01, 9999.99)]
+            public decimal Precio { get; set; }
+        
+    }
+}
